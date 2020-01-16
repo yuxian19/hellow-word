57ee3ae2907b6f564f9b1717252b7797c372fffb
zhouyuxian <yuxian19@163.com>
2020-01-16 23:09:17 +0800

    增加实体类

---
+++ spring-boot/src/main/java/com/example/springboot/dao/UserPojo.java
@@ -0,0 +1,22 @@
+package com.example.springboot.dao;
+
+public class UserPojo {
+    private String name;
+    private String sex;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
